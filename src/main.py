0555a933bd8fd334d614023b056ce707e9124062
Ilya Kazovsky <ilya.kazovsky@wework.com>
2018-08-02 17:12:39 +0300

    First working RPi.GPIO example

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+from time import sleep
 import RPi.GPIO as GPIO
 
-print(GPIO.getmode());
+PIN_NUM = 26
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_NUM, GPIO.OUT)
+GPIO.output(PIN_NUM, GPIO.HIGH)
+sleep(5)
+GPIO.output(PIN_NUM, GPIO.LOW)
